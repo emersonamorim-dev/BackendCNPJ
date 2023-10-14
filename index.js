fd6e910c7ea3866aa4caaa965960e8893335ea6d
Emerson Amorim <emerson_tecno@hotmail.com>
2023-10-14 08:18:47 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 const express = require('express');
 const cors = require('cors');
 const { createProxyMiddleware } = require('http-proxy-middleware');
+const jsonServer = require('json-server');
 
 const app = express();
+const router = jsonServer.router('https://backend-cnpj.vercel.app/');
+const middlewares = jsonServer.defaults();
 
 app.use(cors());
+app.use(middlewares);
 
 app.get('/', (req, res) => {
     console.log('Acessando a rota base');
-    res.type('text/html');  // Definir o tipo de conteúdo explicitamente
+    res.type('text/html');
     res.send('Server is running. Use /v1/cnpj/{cnpj} to access the CNPJ information.');
 });
 
@@ -25,5 +29,12 @@ app.use('/v1/cnpj', (req, res, next) => {
     logLevel: 'debug', 
 }));
 
+// Configuração do json-server
+app.use(jsonServer.rewriter({
+    '/cnpj/*': '/$1',
+}));
+app.use(router);
+
 module.exports = app;
+
 
