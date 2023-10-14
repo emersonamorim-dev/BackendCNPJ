2723069c9bf2e4354c628be7b886d84fc29fcf8e
Emerson Amorim <emerson_tecno@hotmail.com>
2023-10-14 09:27:31 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,34 @@
 const express = require('express');
-const app = express();
+const cors = require('cors');
+const { createProxyMiddleware } = require('http-proxy-middleware');
 const port = process.env.PORT || 3000;
+const app = express();
 
+app.use(cors());
+
+// Middleware para definir o Content-Type como JSON
+app.use((req, res, next) => {
+    res.setHeader('Content-Type', 'application/json');
+    next();
+});
+
+// Rota padrão
 app.get('/', (req, res) => {
-  res.json({ message: 'Test!' });
+    res.json({ message: 'Bem-vindo ao servidor! Use /v1 para acessar o proxy.' });
 });
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
 
-module.exports = app; //
+// Configuração do proxy
+app.use('/v1', createProxyMiddleware({ 
+    target: 'https://www.receitaws.com.br', 
+    changeOrigin: true,
+    onProxyRes: function (proxyRes, req, res) {
+        proxyRes.headers['content-type'] = 'application/json;charset=utf-8';
+    }
+}));
+
+module.exports = app;
+
