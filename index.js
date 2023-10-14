ac64a72cb53092a10b4c4258833e8e6f832f21ad
Emerson Amorim <emerson_tecno@hotmail.com>
2023-10-14 09:11:12 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,32 +2,17 @@ const express = require('express');
 const cors = require('cors');
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
-const app = express();
 const port = process.env.PORT || 3000;
+
+const app = express();
 
 app.use(cors());
 
-app.get('/', (req, res) => {
-    console.log('Acessando a rota base');
-    res.type('text/html');
-    res.send('Server is running. Use /v1/cnpj/{cnpj} to access the CNPJ information.');
-});
-
-// Configuração do proxy
-app.use('/v1/cnpj', (req, res, next) => {
-    console.log('Acessando a rota do proxy');
-    next();
-}, createProxyMiddleware({ 
-    target: 'https://www.receitaws.com.br/v1/cnpj', 
-    changeOrigin: true,
-    pathRewrite: {
-        '^/v1/cnpj': '', 
-    },
-    logLevel: 'debug', 
+app.use('/v1', createProxyMiddleware({ 
+    target: 'https://www.receitaws.com.br', 
+    changeOrigin: true 
 }));
 
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
-});
-
-module.exports = app; 
+  });
