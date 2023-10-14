ec79b1c0584787f22fc0c970aa10d38d2a77b97b
Emerson Amorim <emerson_tecno@hotmail.com>
2023-10-14 09:25:14 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,26 +1,13 @@
 const express = require('express');
-const cors = require('cors');
-
+const app = express();
 const port = process.env.PORT || 3000;
 
-const app = express();
-
-app.use(cors());
-
-// Middleware para definir o Content-Type como JSON
-app.use((req, res, next) => {
-    res.setHeader('Content-Type', 'application/json');
-    next();
-});
-
-// Rota padrão
 app.get('/', (req, res) => {
-    res.json({ message: 'Bem-vindo ao servidor! Use /v1 para acessar o proxy.' });
+  res.json({ message: 'Test!' });
 });
-
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
 
-module.exports = app;
+module.exports = app; //
