f5a477a6920c0184a7ff8b1550884d05953eb0ff
Emerson Amorim <emerson_tecno@hotmail.com>
2023-10-14 08:30:16 -0300

    BackendCNPJ

--- server.js
+++ server.js
@@ -1,17 +1,44 @@
 const express = require('express');
 const cors = require('cors');
 const { createProxyMiddleware } = require('http-proxy-middleware');
+const jsonServer = require('json-server');
 
 const app = express();
+const router = jsonServer.router('https://backend-cnpj.vercel.app/');
+const middlewares = jsonServer.defaults();
 
 app.use(cors());
+app.use(middlewares);
 
-app.use('/v1', createProxyMiddleware({ 
-    target: 'https://www.receitaws.com.br', 
-    changeOrigin: true 
+app.get('/', (req, res) => {
+    console.log('Acessando a rota base');
+    res.type('text/html');
+    res.send('Server is running. Use /v1/cnpj/{cnpj} to access the CNPJ information.');
+});
+
+app.get('/', (req, res) => {
+    const declaredVariable = "Teste!";
+    res.send(declaredVariable);
+});
+
+
+// Configuração do proxy
+app.use('/v1/cnpj', (req, res, next) => {
+    console.log('Acessando a rota do proxy');
+    next();
+}, createProxyMiddleware({ 
+    target: 'https://www.receitaws.com.br/v1/cnpj', 
+    changeOrigin: true,
+    pathRewrite: {
+        '^/v1/cnpj': '', 
+    },
+    logLevel: 'debug', 
 }));
 
-const PORT = 3000;
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
-});
+// Configuração do json-server
+app.use(jsonServer.rewriter({
+    '/cnpj/*': '/$1',
+}));
+app.use(router);
+
+module.exports = app;
